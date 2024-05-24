24154f060970a38b45c34dbb6ba9e580a957523d
Jens Pots <jens@jenspots.com>
2024-05-24 16:59:53 +0200

    feat(test): dummy writers and readers

---
+++ src/test/kotlin/bridge/DummyReader.kt
@@ -0,0 +1,25 @@
+package bridge
+
+class DummyReader(private val contents: Array<ByteArray>) : Reader {
+  private var index = 0
+
+  override suspend fun read(): Reader.Result {
+    return readSync()
+  }
+
+  override fun readSync(): Reader.Result {
+    // If we have read all the contents, return closed.
+    if (isClosed()) {
+      return Reader.Result.closed()
+    }
+
+    // Increment the index and return the next value.
+    val value = contents[index]
+    index += 1
+    return Reader.Result.success(value)
+  }
+
+  override fun isClosed(): Boolean {
+    return index >= contents.size
+  }
+}
