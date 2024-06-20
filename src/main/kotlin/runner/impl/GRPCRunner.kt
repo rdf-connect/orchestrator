3850d7d0138bd15f1c78c55db24608fc6f377da8
Jens Pots <jens@jenspots.com>
2024-06-20 17:18:01 +0200

    feat: grpc implementation

--- src/main/kotlin/runner/impl/GRPCRunner.kt
+++ src/main/kotlin/runner/impl/GRPCRunner.kt
@@ -1,10 +1,77 @@
 package technology.idlab.runner.impl
 
+import EmptyOuterClass.Empty
+import Intermediate as GRPC
 import RunnerGrpcKt
 import io.grpc.ManagedChannelBuilder
 import runner.Runner
+import technology.idlab.parser.intermediate.IRArgument
+import technology.idlab.parser.intermediate.IRParameter
 import technology.idlab.parser.intermediate.IRProcessor
 import technology.idlab.parser.intermediate.IRStage
+
+private val empty = Empty.getDefaultInstance()
+
+private fun IRParameter.Type.toGRPC(): GRPC.IRParameterType {
+  return when (this) {
+    IRParameter.Type.BOOLEAN -> GRPC.IRParameterType.BOOLEAN
+    IRParameter.Type.BYTE -> GRPC.IRParameterType.BYTE
+    IRParameter.Type.DATE -> GRPC.IRParameterType.DATE
+    IRParameter.Type.DOUBLE -> GRPC.IRParameterType.DOUBLE
+    IRParameter.Type.FLOAT -> GRPC.IRParameterType.FLOAT
+    IRParameter.Type.INT -> GRPC.IRParameterType.INT
+    IRParameter.Type.LONG -> GRPC.IRParameterType.LONG
+    IRParameter.Type.STRING -> GRPC.IRParameterType.STRING
+    IRParameter.Type.WRITER -> GRPC.IRParameterType.WRITER
+    IRParameter.Type.READER -> GRPC.IRParameterType.READER
+  }
+}
+
+private fun IRParameter.Presence.toGRPC(): GRPC.IRParameterPresence {
+  return when (this) {
+    IRParameter.Presence.REQUIRED -> GRPC.IRParameterPresence.REQUIRED
+    IRParameter.Presence.OPTIONAL -> GRPC.IRParameterPresence.OPTIONAL
+  }
+}
+
+private fun IRParameter.Count.toGRPC(): GRPC.IRParameterCount {
+  return when (this) {
+    IRParameter.Count.SINGLE -> GRPC.IRParameterCount.SINGLE
+    IRParameter.Count.LIST -> GRPC.IRParameterCount.LIST
+  }
+}
+
+private fun IRParameter.toGRPC(): GRPC.IRParameter {
+  val builder = GRPC.IRParameter.newBuilder()
+  builder.setName(name)
+  builder.setType(type.toGRPC())
+  builder.setPresence(presence.toGRPC())
+  builder.setCount(count.toGRPC())
+  return builder.build()
+}
+
+private fun IRArgument.toGRPC(): GRPC.IRArgument {
+  val builder = GRPC.IRArgument.newBuilder()
+  builder.setName(name)
+  builder.addAllValue(value)
+  return builder.build()
+}
+
+private fun IRStage.toGRPC(): GRPC.IRStage {
+  val builder = GRPC.IRStage.newBuilder()
+  builder.setUri(uri)
+  builder.setProcessorUri(processor.uri)
+  builder.addAllArguments(arguments.map { it.toGRPC() })
+  return builder.build()
+}
+
+private fun IRProcessor.toGRPC(): GRPC.IRProcessor {
+  val builder = GRPC.IRProcessor.newBuilder()
+  builder.setUri(uri)
+  builder.addAllParameters(parameters.map { it.toGRPC() })
+  builder.putAllMetadata(metadata)
+  return builder.build()
+}
 
 /**
  * This runner has GRPC built-in, so the only configuration that an extending class needs to provide
@@ -23,20 +90,20 @@ abstract class GRPCRunner(host: String, port: Int) : Runner() {
   }
 
   override suspend fun prepare(processor: IRProcessor) {
-    TODO("Not yet implemented.")
+    grpc.prepareProcessor(processor.toGRPC())
   }
 
   override suspend fun prepare(stage: IRStage) {
-    TODO("Not yet implemented.")
+    grpc.prepareStage(stage.toGRPC())
   }
 
   override suspend fun exec() {
-    TODO("Not yet implemented.")
+    grpc.exec(empty)
   }
 
   override fun halt() {
     super.halt()
-    TODO("Not yet implemented.")
+    process.destroy()
   }
 
   override suspend fun status(): Status {
