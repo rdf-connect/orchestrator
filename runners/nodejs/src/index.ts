43818cb70b63248ca564118bf52b3388777934ff
Jens Pots <jens@jenspots.com>
2024-07-08 15:14:09 +0200

    feat: default export in node runner

---
+++ runners/nodejs/src/index.ts
@@ -0,0 +1,6 @@
+import { Arguments } from "./runtime/arguments";
+import { Reader } from "./interfaces/reader";
+import { Writer } from "./interfaces/writer";
+import { Processor } from "./interfaces/processor";
+
+export { Arguments, Reader, Writer, Processor };
