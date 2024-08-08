0ca4571b6f6e6a163555e364bc81d55d73daa00d
Jens Pots <jens@jenspots.com>
2024-08-08 18:33:04 +0200

    fix(runner-py): add protobuf path at runtime due to compiler issues

--- packages/runner-py/src/rdfc/__init__.py
+++ packages/runner-py/src/rdfc/__init__.py
@@ -1,9 +1,16 @@
 import asyncio
-
+import pathlib
+import sys
 from rdfc.runtime import Reader, Writer, Processor, Arguments
 
 
 def main():
+    src_file = pathlib.Path(__file__).resolve()
+    src_dir = src_file.parent
+    proto_dir = f"{src_dir}/proto"
+
+    sys.path.append(proto_dir)
+
     async def async_main():
         import rdfc.grpc
 
