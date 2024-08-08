3e7c764928f55954df1a0a32079dd4ed3db27ae4
Jens Pots <jens@jenspots.com>
2024-08-08 18:14:27 +0200

    fix: generate proto files separately for python

--- packages/runner-py/src/rdfc/proto/index_pb2.pyi
+++
@@ -1,7 +0,0 @@
-import rdfc.proto.channel_pb2 as _channel_pb2
-import rdfc.proto.intermediate_pb2 as _intermediate_pb2
-from google.protobuf import empty_pb2 as _empty_pb2
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-DESCRIPTOR: _descriptor.FileDescriptor
