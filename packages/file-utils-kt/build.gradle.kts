f32a43efb68ae7a0ba6e0ad0c20207cf062e2db7
Jens Pots <jens@jenspots.com>
2024-09-12 12:56:07 +0200

    refactor: moved orchestrator to separate dir [no ci]

--- packages/file-utils-kt/build.gradle.kts
+++ packages/file-utils-kt/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
   implementation("org.reflections:reflections:0.10.2")
 
   // Processor class and other utilities.
-  implementation(files("../../build/libs/technology.idlab.jvm-runner-0.0.4-all.jar"))
+  implementation(files("../../orchestrator/build/libs/technology.idlab.jvm-runner-0.0.4-all.jar"))
 
   // RDF dependencies.
   implementation("org.apache.jena:apache-jena-libs:5.0.0")
