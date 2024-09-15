2ee5f2316f9dfe36017b845ce86f3720d24c77ec
Jens Pots <jens@jenspots.com>
2024-09-15 18:54:04 +0200

    refactor: reformat typescript globally

--- format.sh
+++ format.sh
@@ -2,31 +2,36 @@
 
 TRACKED_FILES="$(git ls-files --cached --others --exclude-standard)"
 
-function find_tracked_files() {
-    echo "$TRACKED_FILES" | grep "$1$" | tee /dev/tty
+function grep_tracked_files() {
+    echo "$TRACKED_FILES" | grep "$1" | tee /dev/tty
 }
 
 echo "Formatting: Kotlin"
-find_tracked_files ".kt" | xargs ktfmt 2>/dev/null
-find_tracked_files ".kts" | xargs ktfmt 2>/dev/null
+grep_tracked_files "\.kt$" | xargs ktfmt 2>/dev/null
+grep_tracked_files "\.kts$" | xargs ktfmt 2>/dev/null
 echo ""
 
 echo "Formatting: Java"
-find_tracked_files ".java" | xargs -I "{}" google-java-format -r "{}"
+grep_tracked_files "\.java$" | xargs -I "{}" google-java-format -r "{}"
 echo ""
 
 echo "Formatting: YAML"
-find_tracked_files ".yml" | xargs -I "{}" yamlfmt "{}"
-find_tracked_files ".yaml" | xargs -I "{}" yamlfmt "{}"
+grep_tracked_files "\.yml$" | xargs -I "{}" yamlfmt "{}"
+grep_tracked_files "\.yaml$" | xargs -I "{}" yamlfmt "{}"
 echo ""
 
 echo "Formatting: Markdown"
-find_tracked_files ".md" | xargs -I "{}" mdformat "{}"
+grep_tracked_files "\.md$" | xargs -I "{}" mdformat "{}"
 echo ""
 
 echo "Formatting: Python"
-find_tracked_files ".py" | xargs black
+grep_tracked_files "\.py$" | xargs black
 echo ""
 
 echo "Formatting: Protobuf"
-find_tracked_files ".proto" | xargs -I "{}" buf format -w "{}"
+grep_tracked_files "\.proto$" | xargs -I "{}" buf format -w "{}"
+echo ""
+
+echo "Formatting: JavaScript/TypeScript"
+grep_tracked_files "\.js$" | xargs npx prettier --write >/dev/null
+grep_tracked_files "\.ts$" | xargs npx prettier --write >/dev/null
