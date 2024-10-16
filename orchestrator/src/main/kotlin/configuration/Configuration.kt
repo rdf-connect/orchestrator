ac26d0c638630be60c6d7f179429948f997d0394
Jens Pots <jens@jenspots.com>
2024-10-16 21:04:10 +0200

    refactor: remove unused `config` package

--- orchestrator/src/main/kotlin/configuration/Configuration.kt
+++
@@ -1,9 +0,0 @@
-package technology.idlab.configuration
-
-import technology.idlab.parser.Parser
-
-/**
- * The Configuration class is an extension of the parser, and allows for much more powerful querying
- * of the model, as well as provide important assertions of the model's consistency.
- */
-abstract class Configuration(parser: Parser) {}
