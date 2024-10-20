5eae131c5f5ce11c220b2ad62b5c3f430987e440
Jens Pots <jens@jenspots.com>
2024-10-20 18:00:42 +0200

    fix: update root project name [no ci]

--- orchestrator/settings.gradle.kts
+++ orchestrator/settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0" }
 
-rootProject.name = "technology.idlab.jvm-runner"
+rootProject.name = "technology.idlab.rdfc"
 
 include("rdfc-core")
 
