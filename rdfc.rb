604a75ccaf9242cb86f1640047ae61274e7bbcff
Jens Pots <jens@jenspots.com>
2024-11-10 12:26:10 +0100

    chore: update `brew` package [no ci]

--- rdfc.rb
+++ rdfc.rb
@@ -1,18 +1,18 @@
 class Rdfc < Formula
   desc "RDC-Connect orchestrator."
   homepage "https://github.com/rdfc-connect/orchestrator"
-  url "https://github.com/rdf-connect/orchestrator/releases/download/v0.0.1/rdfc-orchestrator-0.0.1.jar"
-  sha256 "d01643c46a413a10cabf5eb07a53e0c941b4549c21e3203ee760dd1fd90a4e44"
-  version "0.0.1"
+  url "https://github.com/rdf-connect/orchestrator/releases/download/v0.0.2/rdfc-cli-0.0.2.jar"
+  sha256 "1195c8fd7d7c8518413e967b8ecbd157f0d12b63648ddb0b63823ab62995738a"
+  version "0.0.2"
   license "MIT"
 
   depends_on "openjdk"
 
   def install
-    libexec.install "rdfc-orchestrator-0.0.1.jar"
+    libexec.install "rdfc-cli-0.0.2.jar"
     (bin/"rdfc").write <<~EOS
       #!/bin/bash
-      exec java -jar #{libexec}/rdfc-orchestrator-0.0.1.jar "$@"
+      exec java -jar #{libexec}/rdfc-cli-0.0.2.jar "$@"
     EOS
   end
 
