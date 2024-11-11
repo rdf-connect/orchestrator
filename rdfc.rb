ada0f81d9b633a5380b6e38babab742e04ee4e22
Jens Pots <jens@jenspots.com>
2024-11-11 16:59:33 +0100

    chore(brew): bump to `v.0.0.3`

--- rdfc.rb
+++ rdfc.rb
@@ -1,18 +1,18 @@
 class Rdfc < Formula
   desc "RDC-Connect orchestrator."
   homepage "https://github.com/rdfc-connect/orchestrator"
-  url "https://github.com/rdf-connect/orchestrator/releases/download/v0.0.2/rdfc-cli-0.0.2.jar"
-  sha256 "1195c8fd7d7c8518413e967b8ecbd157f0d12b63648ddb0b63823ab62995738a"
-  version "0.0.2"
+  url "https://github.com/rdf-connect/orchestrator/releases/download/v0.0.3/rdfc-cli-0.0.3.jar"
+  sha256 "0ec119eacda77573063ab7257322e1660115f7e6ee4556029b52303b23168b2f"
+  version "0.0.3"
   license "MIT"
 
   depends_on "openjdk"
 
   def install
-    libexec.install "rdfc-cli-0.0.2.jar"
+    libexec.install "rdfc-cli-0.0.3.jar"
     (bin/"rdfc").write <<~EOS
       #!/bin/bash
-      exec java -jar #{libexec}/rdfc-cli-0.0.2.jar "$@"
+      exec java -jar #{libexec}/rdfc-cli-0.0.3.jar "$@"
     EOS
   end
 
